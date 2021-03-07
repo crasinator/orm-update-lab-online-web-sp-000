641649e7bd22a9f1c774ad2113c9cae56f35aef9
Jordan Reed <crasinator@gmail.com>
2021-03-06 22:06:11 -0500

    commit tree 995708fa15442fc7cce57c02228468776ba3f24a

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,6 @@ class Student
     @id = id 
     @name = name 
     @grade = grade 
-    
   end 
 
   def self.create_table 
@@ -44,12 +43,10 @@ class Student
   def self.create(name, grade)
     var = self.new(name, grade)
     var.save
-    var
   end 
 
   def self.new_from_db(row)
     student = self.new(row[0], row[1], row[2])
-    student
   end
 
   def self.find_by_name(name)
